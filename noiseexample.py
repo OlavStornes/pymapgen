8be00b95fc006366619255ab03f88f3f236f7435
OlavStornes <ost028@post.uit.no>
2018-07-22 23:07:12 +0200

    Separated elevation generation and image saving

--- noiseexample.py
+++ noiseexample.py
@@ -11,17 +11,25 @@ def createelevation():
     simplex = OpenSimplex()
 
     print('Generating noise from OpenSimplex...')
+    elevation = [[0 for x in range(WIDTH)] for y in range(HEIGHT)] 
+    for y in range(0, HEIGHT):
+        for x in range(0, WIDTH):
+            elevation[y][x] = simplex.noise2d(x / FEATURE_SIZE, y / FEATURE_SIZE)
+    return elevation
+
+
+def saveimg(elevation):
+    print ("Saving Image...")
     im = Image.new('L', (WIDTH, HEIGHT))
     for y in range(0, HEIGHT):
         for x in range(0, WIDTH):
-            elevation = simplex.noise2d(x / FEATURE_SIZE, y / FEATURE_SIZE)
-            color = int((elevation + 1) * 128)
+            color = int((elevation[x][y] + 1) * 128)
             im.putpixel((x, y), color)
     im.save('noise.png')
 
-def saveimg(elevation):
-    pass
-
+def main():
+    elevation = createelevation()
+    saveimg(elevation)
 
 if __name__ == '__main__':
     main()
